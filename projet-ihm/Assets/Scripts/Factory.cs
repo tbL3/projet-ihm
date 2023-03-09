8066b7dedf7ba605cee11973f211f988dffa491c
thomasGarau <thomas.garau8@gmail.com>
2023-03-09 19:02:46 +0100

    create project structure + create factory spirites and script

--- projet-ihm/Assets/Scripts/Factory.cs
+++ projet-ihm/Assets/Scripts/Factory.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class Factory : MonoBehaviour
 {
+    private double positionX = -9.48;
+    private double positionY = -3.54;
+    private double scaleX = 0.4;
+    private double scaleY = 0.4;
+
     // Start is called before the first frame update
+
     void Start()
     {
         
+    }
+
+    void Awake()
+    {
+        Debug.Log("hello World");
     }
 
     // Update is called once per frame
